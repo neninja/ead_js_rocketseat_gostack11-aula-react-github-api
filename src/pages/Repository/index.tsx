29479800eea9e90f5bd5fd8b3c4bd9d9035acfeb
Felipe Silva <felipe.dacs@gmail.com>
2020-04-25 02:50:10 -0300

    release: add aula 12

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="http://github.com/nenitf.png" alt="" />
+          <div>
+            <strong>nenitf/dotfiles</strong>
+            <p>descrição do projeto</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>0</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>1</strong>
+            <span>forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aaa">
+          <div>
+            <strong>dgysgdyg</strong>
+            <p>hdsgds</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
